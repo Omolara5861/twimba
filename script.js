46912699b167b0d321881f9dbc6c6df8f258b6e6
Omolara5861 <debbiegterra@gmail.com>
2022-10-04 21:27:16 +0100

    feat: :sparkles: rendered tweet comments on a page

--- script.js
+++ script.js
@@ -171,7 +171,48 @@
             if(tweet.isRetweeted) {
                 retweetIconClass = 'retweeted';
             }
-                    feedHtml += `
+
+            let repliesHtml = '';
+            /*
+            Challenge:
+            1. Use an if statement to check if a tweet has replies.
+            2. If it does, log out the uuid for that tweet.
+            */
+            /*
+            Challenge:
+            1. If a tweet has replies, iterate through the replies
+            and wrap each one in the HTML template provided below. 
+            Make sure to replace words in UPPERCASE with data from 
+            the tweet. On each iteration, add this HTML to repliesHtml.
+            
+            <div class="tweet-reply">
+                <div class="tweet-inner">
+                    <img src="PROFILE PIC" class="profile-pic">
+                        <div>
+                            <p class="handle">HANDLE</p>
+                            <p class="tweet-text">TWEET TEXT</p>
+                        </div>
+                    </div>
+            </div>
+            */
+            if (tweet.replies.length > 0) {
+                // console.log(tweet.uuid);
+                tweet.replies.forEach(reply => {
+                    repliesHtml += `
+                    <div class="tweet-reply">
+                    <div class="tweet-inner">
+                        <img src="${reply.profilePic}" class="profile-pic">
+                            <div>
+                                <p class="handle">${reply.handle}</p>
+                                <p class="tweet-text">${reply.tweetText}</p>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                })
+            }
+
+            feedHtml += `
             <div class="tweet">
             <div class="tweet-inner">
                 <img src="${tweet.profilePic}" class="profile-pic">
@@ -194,8 +235,17 @@
                     </div>   
                 </div>            
             </div>
+            <div class="hidden" id="replies-${tweet.uuid}">
+            <!-- REPLIES HERE -->
+            ${repliesHtml}
+            </div>   
         </div>
             `;
+            /*
+            Challenge:
+            2. Place repliesHtml in its parent div remembering 
+            to update that divs id.
+            */
         });
         return feedHtml;
     }
